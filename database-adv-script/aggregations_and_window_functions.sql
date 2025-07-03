de727c882cab1f559b5d11496e06e3971105f5a5
Emanuel1997 <osomisakin@gmail.com>
2025-07-03 17:19:33 +0100

    total number of bookings made by each user, using the COUNT function and GROUP BY clause.

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,8 @@
+--total number of bookings made by each user, using the COUNT function and GROUP BY clause.
+SELECT 
+  u.user_id,
+  u.first_name || ' ' || u.last_name AS full_name,
+  COUNT(b.booking_id) AS total_bookings
+FROM Users u
+JOIN Booking b ON u.user_id = b.user_id
+GROUP BY u.user_id, u.first_name, u.last_name;
